9f7bd456ad64f66d69821cf416de079d3b8d834d
Vaibhav <vaibhavsiroha2000@gmail.com>
2025-05-28 20:04:04 +0530

    Add custom user admin and initialize matches app structure

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Users
 
+class CustomUserAdmin(UserAdmin):
+    model = Users
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('bio', 'age', 'gender', 'location', 'profile_pic')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('bio', 'age', 'gender', 'location', 'profile_pic')}),
+    )
 
-admin.site.register(Users,UserAdmin)
-# Register your models here.
+admin.site.register(Users, CustomUserAdmin)
+
